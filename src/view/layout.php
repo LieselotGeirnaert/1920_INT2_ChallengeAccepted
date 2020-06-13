296ef90fe0dda414bb37d7202810d7a2f3e7e85d
Lieselot Geirnaert <lieselot.geirnaert@gmail.com>
2020-06-13 23:31:30 +0200

    add styling hoehinderen wip

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,9 +4,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <!-- <link rel="stylesheet" href="css/reset.css"> -->
+  <link rel="stylesheet" href="https://use.typekit.net/gpi1ppr.css">
   <link rel="stylesheet" href="css/style.css">
-  <!-- <link rel="stylesheet" href="css/home.css"> -->
   <title>HotelHinder - <?php echo $title;?></title>
 </head>
 <body>
--- src/view/pages/hoehinderen.php
+++ src/view/pages/hoehinderen.php
@@ -1,40 +1,67 @@
 <section>
   <h2 class="hidden">Hoe hinderen</h2>
-  <section class="why">
-    <h3>Waarom HotelHinder</h3>
-    <p>We zien het als <span class="accent">onze eigen missie om de wereld een beetje mooier te maken</span>,
+
+  <section class="why content">
+    <h3 class="why__title">Waarom HotelHinder</h3>
+    <p class="why__text">We zien het als <span class="why__text--accent">onze eigen missie om de wereld een beetje mooier te maken</span>,
         en daarvoor hebben we de hulp nodig van alle stiekeme plaaggeesten en pestkoppen, 
-        jullie dus. <span class="accent">Samen kan je je mede-hotelgasten de ultieme hinder-ervaring laten
+        jullie dus. <span class="why__text--accent">Samen kan je je mede-hotelgasten de ultieme hinder-ervaring laten
         beleven</span> en zo hun reis helemaal perfect maken.
     </p>
   </section>
+
   <section class="how">
-    <h3>Zet het hotel op stelten</h3>
-    <p>Ga aan de slag met hinderen en start jouw hinder-ervaring in 3 gemakkelijk stappen.</p>
-    <article>
-      <h4>1</h4>
-      <p>Kies tijdens je all-in reis jouw favoriete hinder-locatie of -situatie uit ons aanbod.
-        Zowel in het hotel zelf, als tijdens de heen- en terugreis kan je al aan de slag gaan met hinderen.
-      </p>
-    </article>
-    <article>
-      <h4>2</h4>
-      <p>Hinder zo veel mogelijk mensen rondom je en maak er een filmpje van. Upload het filmpje zodat anderen kunnen meegenieten van alle heisa.
-      </p>
-    </article>
-    <article>
-      <h4>3</h4>
-      <p>Verzamel zo veel mogelijk goede recensies en reacties van je dankbare slachtoffers zodat jij de irritantste hotelbezoeker wordt.
-      </p>
-    </article>
+    <h3 class="subtitle how__subtitle">Zet het hotel op stelten</h3>
+    <p class="how__text">Ga aan de slag met hinderen en start jouw hinder-ervaring in 3 gemakkelijk stappen.</p>
+    <div class="how__scrollcontainer">
+      <div class="how__container">
+        <article class="container__card container__card--one">
+          <h4 class="card__number"><span class="hidden">1</span></h4>
+          <p class="card__text">Kies tijdens je all-in reis jouw favoriete hinder-locatie of -situatie uit ons aanbod.
+            Zowel in het hotel zelf, als tijdens de heen- en terugreis kan je al aan de slag gaan met hinderen.
+          </p>
+        </article>
+        <article class="container__card container__card--two">
+          <h4 class="card__number"><span class="hidden">2</span></h4>
+          <p class="card__text">Hinder zo veel mogelijk mensen rondom je en maak er een filmpje van. Upload het filmpje zodat anderen kunnen meegenieten van alle heisa.
+          </p>
+        </article>
+        <article class="container__card container__card--three">
+          <h4 class="card__number"><span class="hidden">3</span></h4>
+          <p class="card__text">Verzamel zo veel mogelijk goede recensies en reacties van je dankbare slachtoffers zodat jij de irritantste hotelbezoeker wordt.
+          </p>
+        </article>
+      </div>
+    </div>
+    <div class="how__arrows">
+      <img src="assets/img/arrow-yellow.png" class="arrows__icon arrows__icon--left" alt="scroll arrow left">
+      <img src="assets/img/arrow-yellow.png" class="arrows__icon" alt="scroll arrow right">
+    </div>
+    <button class="btn">Begin met hinderen</button>
   </section>
-  <section class="tips">
-    <h3>All-in tips</h3>
+
+  <section class="tips content">
+    <h3 class="subtitle tips__subtitle">All-in tips</h3>
     <section class="tips__cocktails">
       <h4>Cocktails tijdens het hinderen</h4>
       <p>Met een cocktail bij de hand gaat het hinderen nog beter. Met deze 3 toppers bezorg je ook de barman een extra frustratie. Ontdek onze favorieten en geniet mee.</p>
+      <div class="cocktails__container">
+        <article class="container__cocktail container__cocktail--mojito">
+          <h5 class="cocktail__name">Mojito</h5>
+          <p class="cocktail__descr">Wij houden wel van een beetje suiker in onze cocktail, barmannen zijn minder fan van hun plakhandjes.</p>
+        </article>
+        <article class="container__cocktail container__cocktail--icedtea">
+          <h5 class="cocktail__name">Long island iced tea</h5>
+          <p class="cocktail__descr">Talloze ingrediënten die allemaal precies afgemeten moeten worden, geen lachertje op een drukke dag.</p>
+        </article>
+        <article class="container__cocktail container__cocktail--margarita">
+          <h5 class="cocktail__name">Margarita</h5>
+          <p class="cocktail__descr">Geen moeilijke ingrediënten maar wel 20 seconden lang schudden, daar heeft geen enkele barman tijd voor.</p>
+        </article>
+      </div>
     </section>
-     <section class="tips__music">
+
+    <section class="tips__music">
       <h4>Muziekmanie</h4>
       <p>Geniet zelf van een streepje muziek tijdens het hinderen, of gebruik de muziek om anderen te irriteren.</p>
     </section>
